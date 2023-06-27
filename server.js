ae2e4d6305f8e652d5196aeb62bcffe74cd3a5da
ansar <ansarahmedcjc17@gmail.com>
2023-06-27 23:56:12 +0800

    test commit

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use("/users", usersRouter);
 app.use("/epfs", epfRouter);
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000.");
 });
