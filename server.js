7475dfbd7c28b92f3583c234b5b1ccf3010ece46
John Loh <johnlohjy@gmail.com>
2023-07-12 22:01:43 +0800

    updated server code

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 Logic for Server
 */
 
-import express from "express";
-const app = express();
-app.use(express.json());
-import usersRouter from "./routes/user.js";
-import epfRouter from "./routes/epf.js";
+import express from 'express'
+const app = express()
+app.use(express.json())
+import excoRouter from './routes/exco.js'
+import epfRouter from './routes/epf.js'
 
-app.use("/users", usersRouter);
-app.use("/epfs", epfRouter);
+app.use('/users', excoRouter)
+app.use('/epfs', epfRouter)
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
-});
+app.listen(3000, ()=> {
+    console.log('Server is running on port 3000')
+})
