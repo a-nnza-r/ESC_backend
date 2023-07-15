708db320c7b33aa9866b756eff2e71a897aa837e
ansar <ansarahmedcjc17@gmail.com>
2023-07-15 16:46:20 +0800

    fixing commits

--- models/epf_db.js
+++ models/epf_db.js
@@ -15,12 +15,11 @@ const credentials = {
 export async function count_outstanding_EPF() {
   const pool = new Pool(credentials);
   const result = await pool.query(
-    `SELECT COUNT(*) FROM EPFS WHERE status !='Approved'`
+    `SELECT COUNT(*) FROM EPFS WHERE status != $1`,
+    ["Approved"]
   );
-
-  await pool.query(
-    `UPDATE EXCO SET outstanding_epf='${result["rows"][0]["count"]}';`
-  );
+  const updateQuery = `UPDATE EXCO SET outstanding_epf=$1`;
+  await pool.query(updateQuery, [result["rows"][0]["count"]]);
   await pool.end();
 
   //for testing
@@ -112,6 +111,112 @@ export async function createEPF(
   G_comments_ROOT
 ) {
   const pool = new Pool(credentials);
+  const columnParams = new Array(82)
+    .fill()
+    .map((_, i) => `$${i + 1}`)
+    .join(",");
+  const values = [
+    status,
+    exco_user_id,
+    A_name,
+    A_student_id,
+    A_organisation,
+    A_contact_number,
+    A_email,
+    A_comments_OSL,
+    A_comments_ROOT,
+
+    B_event_name,
+    B_target_audience,
+    B_event_schedule,
+    B_expected_turnout,
+    B_event_objective,
+    B_comments_OSL,
+    B_comments_ROOT,
+
+    C1_date,
+    C1_time,
+    C1_activity_and_description,
+    C1_venue,
+
+    C2_date,
+    C2_time,
+    C2_activity_and_description,
+    C2_venue,
+
+    C3_date,
+    C3_time,
+    C3_activity_and_description,
+    C3_venue,
+
+    C3_cleanup_date,
+    C3_cleanup_time,
+    C3_cleanup_activity_and_description,
+    C3_cleanup_venue,
+    C_comments_OSL,
+    C_comments_ROOT,
+
+    D1A_club_income_fund,
+    D1A_osl_seed_fund,
+    D1A_donation,
+    D1B_revenue,
+    D1B_donation_or_scholarship,
+    D1B_total_source_of_funds,
+
+    D11_items_goods_services,
+    D11_price,
+    D11_quantity,
+    D11_amount,
+    D11_total_revenue,
+
+    D2_items,
+    D2_reason_for_purchase,
+    D2_venue,
+    D2_total_expenditure,
+    D_comments_OSL,
+    D_comments_ROOT,
+
+    E_personal_data,
+    E_comments_OSL,
+    E_comments_ROOT,
+
+    F_name,
+    F_student_id,
+    F_position,
+    F_comments_OSL,
+    F_comments_ROOT,
+
+    G_1_1,
+    G_1_2,
+    G_1_3,
+
+    G_2_1,
+    G_2_2,
+    G_2_3,
+
+    G_3_1,
+    G_3_2,
+    G_3_3,
+
+    G_4_1,
+    G_4_2,
+    G_4_3,
+
+    G_5_1,
+    G_5_2,
+    G_5_3,
+
+    G_6_1,
+    G_6_2,
+    G_6_3,
+
+    G_7_1,
+    G_7_2,
+    G_7_3,
+    G_comments_OSL,
+    G_comments_ROOT,
+  ];
+
   const metadata = "status,exco_user_id,";
   const sectionA =
     "A_name,A_student_id,A_organisation,A_contact_number,A_email,A_comments_OSL,A_comments_ROOT,";
@@ -159,15 +264,21 @@ export async function createEPF(
     sectionG6,
     sectionG7
   );
-  const query = `INSERT INTO EPFS (${column_names}) VALUES ('${status}', ${exco_user_id},'${A_name}', ${A_student_id}, '${A_organisation}', ${A_contact_number}, '${A_email}', '${A_comments_OSL}', '${A_comments_ROOT}', '${B_event_name}', '${B_target_audience}', '${B_event_schedule}', ${B_expected_turnout}, '${B_event_objective}', '${B_comments_OSL}', '${B_comments_ROOT}', ARRAY ${C1_date}, ARRAY ${C1_time}, ARRAY ${C1_activity_and_description}, ARRAY ${C1_venue},ARRAY ${C2_date}, ARRAY ${C2_time}, ARRAY ${C2_activity_and_description}, ARRAY ${C2_venue},ARRAY ${C3_date}, ARRAY ${C3_time}, ARRAY ${C3_activity_and_description}, ARRAY ${C3_venue},ARRAY ${C3_cleanup_date}, ARRAY ${C3_cleanup_time}, ARRAY ${C3_cleanup_activity_and_description}, ARRAY ${C3_cleanup_venue}, '${C_comments_OSL}', '${C_comments_ROOT}', ${D1A_club_income_fund}, ${D1A_osl_seed_fund}, ${D1A_donation}, ${D1B_revenue}, ${D1B_donation_or_scholarship}, ${D1B_total_source_of_funds},ARRAY ${D11_items_goods_services}, ARRAY ${D11_price}, ARRAY ${D11_quantity}, ARRAY ${D11_amount}, ${D11_total_revenue},ARRAY ${D2_items}, ARRAY ${D2_reason_for_purchase}, ARRAY ${D2_venue}, ${D2_total_expenditure}, '${D_comments_OSL}', '${D_comments_ROOT}', ${E_personal_data}, '${E_comments_OSL}', '${E_comments_ROOT}', ARRAY ${F_name}, ARRAY ${F_student_id}, ARRAY ${F_position}, '${F_comments_OSL}','${F_comments_ROOT}','${G_1_1}', '${G_1_2}', '${G_1_3}','${G_2_1}', '${G_2_2}', '${G_2_3}','${G_3_1}', '${G_3_2}', '${G_3_3}','${G_4_1}', '${G_4_2}', '${G_4_3}','${G_5_1}', '${G_5_2}', '${G_5_3}','${G_6_1}', '${G_6_2}', '${G_6_3}','${G_7_1}', '${G_7_2}', '${G_7_3}', '${G_comments_OSL}','${G_comments_ROOT}');`;
-  await pool.query(query);
+
+  const query = `INSERT INTO EPFS(${column_names}) VALUES (${columnParams})`;
+  await pool.query(query, values);
   await pool.end();
 }
 
 export async function getEPF(epf_id) {
   const pool = new Pool(credentials);
-  const result = await pool.query(`SELECT * FROM EPFS WHERE epf_id=${epf_id}`);
+  const result = await pool.query("SELECT * FROM EPFS WHERE epf_id=$1", [
+    epf_id,
+  ]);
   await pool.end();
+  if (result.rowCount === 0) {
+    return null;
+  }
   return result["rows"];
 }
 
@@ -264,14 +375,108 @@ export async function updateEPF(
   G_comments_ROOT
 ) {
   const pool = new Pool(credentials);
-  const query = `UPDATE EPFS SET status='${status}', exco_user_id=${exco_user_id}, A_name='${A_name}', A_student_id=${A_student_id}, A_organisation='${A_organisation}', A_contact_number=${A_contact_number}, A_email='${A_email}', A_comments_OSL='${A_comments_OSL}', A_comments_ROOT='${A_comments_ROOT}', B_event_name='${B_event_name}', B_target_audience='${B_target_audience}', B_event_schedule='${B_event_schedule}', B_expected_turnout=${B_expected_turnout}, B_event_objective='${B_event_objective}', B_comments_OSL='${B_comments_OSL}',B_comments_ROOT='${B_comments_ROOT}', C1_date=ARRAY ${C1_date}, C1_time=ARRAY ${C1_time}, C1_activity_and_description=ARRAY ${C1_activity_and_description}, C1_venue=ARRAY ${C1_venue},C2_date=ARRAY ${C2_date}, C2_time=ARRAY ${C2_time}, C2_activity_and_description=ARRAY ${C2_activity_and_description}, C2_venue=ARRAY ${C2_venue},C3_date=ARRAY ${C3_date}, C3_time=ARRAY ${C3_time}, C3_activity_and_description=ARRAY ${C3_activity_and_description}, C3_venue=ARRAY ${C3_venue},C3_cleanup_date=ARRAY ${C3_cleanup_date}, C3_cleanup_time=ARRAY ${C3_cleanup_time}, C3_cleanup_activity_and_description=ARRAY ${C3_cleanup_activity_and_description}, C3_cleanup_venue=ARRAY ${C3_cleanup_venue}, C_comments_OSL='${C_comments_OSL}', C_comments_ROOT='${C_comments_ROOT}', D1A_club_income_fund=${D1A_club_income_fund}, D1A_osl_seed_fund=${D1A_osl_seed_fund}, D1A_donation=${D1A_donation}, D1B_revenue=${D1B_revenue}, D1B_donation_or_scholarship=${D1B_donation_or_scholarship}, D1B_total_source_of_funds=${D1B_total_source_of_funds},D11_items_goods_services=ARRAY ${D11_items_goods_services}, D11_price=ARRAY ${D11_price}, D11_quantity=ARRAY ${D11_quantity}, D11_amount=ARRAY ${D11_amount}, D11_total_revenue=${D11_total_revenue},D2_items=ARRAY ${D2_items}, D2_reason_for_purchase=ARRAY ${D2_reason_for_purchase}, D2_venue=ARRAY ${D2_venue}, D2_total_expenditure=${D2_total_expenditure}, D_comments_OSL='${D_comments_OSL}', D_comments_ROOT='${D_comments_ROOT}', E_personal_data=${E_personal_data}, E_comments_OSL='${E_comments_OSL}', E_comments_ROOT='${E_comments_ROOT}', F_name=ARRAY ${F_name}, F_student_id=ARRAY ${F_student_id}, F_position=ARRAY ${F_position}, F_comments_OSL = '${F_comments_OSL}', F_comments_ROOT = '${F_comments_ROOT}', G_1_1='${G_1_1}', G_1_2='${G_1_2}', G_1_3='${G_1_3}',G_2_1='${G_2_1}', G_2_2='${G_2_2}', G_2_3='${G_2_3}',G_3_1='${G_3_1}', G_3_2='${G_3_2}', G_3_3='${G_3_3}',G_4_1='${G_4_1}', G_4_2='${G_4_2}', G_4_3='${G_4_3}',G_5_1='${G_5_1}', G_5_2='${G_5_2}', G_5_3='${G_5_3}',G_6_1='${G_6_1}', G_6_2='${G_6_2}', G_6_3='${G_6_3}',G_7_1='${G_7_1}', G_7_2='${G_7_2}', G_7_3='${G_7_3}', G_comments_OSL='${G_comments_OSL}', G_comments_ROOT='${G_comments_ROOT}' WHERE epf_id=${epf_id};`;
-  await pool.query(query);
+  const columnNames =
+    "status,exco_user_id,A_name,A_student_id,A_organisation,A_contact_number,A_email,A_comments_OSL,A_comments_ROOT,B_event_name,B_target_audience,B_event_schedule,B_expected_turnout,B_event_objective,B_comments_OSL,B_comments_ROOT,C1_date,C1_time,C1_activity_and_description,C1_venue,C2_date,C2_time,C2_activity_and_description,C2_venue,C3_date,C3_time,C3_activity_and_description,C3_venue,C3_cleanup_date,C3_cleanup_time,C3_cleanup_activity_and_description,C3_cleanup_venue,C_comments_OSL,C_comments_ROOT,D1A_club_income_fund,D1A_osl_seed_fund,D1A_donation,D1B_revenue,D1B_donation_or_scholarship,D1B_total_source_of_funds,D11_items_goods_services,D11_price,D11_quantity,D11_amount,D11_total_revenue,D2_items,D2_reason_for_purchase,D2_venue,D2_total_expenditure,D_comments_OSL,D_comments_ROOT,E_personal_data,E_comments_OSL,E_comments_ROOT,F_name,F_student_id,F_position,F_comments_OSL,F_comments_ROOT, G_1_1,G_1_2,G_1_3,G_2_1,G_2_2,G_2_3,G_3_1,G_3_2,G_3_3,G_4_1,G_4_2,G_4_3,G_5_1,G_5_2,G_5_3,G_6_1,G_6_2,G_6_3,G_7_1,G_7_2,G_7_3,G_comments_OSL,G_comments_ROOT";
+  const columnParams = columnNames
+    .split(",")
+    .map((_, i) => `$${i + 2}`)
+    .join(",");
+  const values = [
+    epf_id,
+    status,
+    exco_user_id,
+    A_name,
+    A_student_id,
+    A_organisation,
+    A_contact_number,
+    A_email,
+    A_comments_OSL,
+    A_comments_ROOT,
+    B_event_name,
+    B_target_audience,
+    B_event_schedule,
+    B_expected_turnout,
+    B_event_objective,
+    B_comments_OSL,
+    B_comments_ROOT,
+    C1_date,
+    C1_time,
+    C1_activity_and_description,
+    C1_venue,
+    C2_date,
+    C2_time,
+    C2_activity_and_description,
+    C2_venue,
+    C3_date,
+    C3_time,
+    C3_activity_and_description,
+    C3_venue,
+    C3_cleanup_date,
+    C3_cleanup_time,
+    C3_cleanup_activity_and_description,
+    C3_cleanup_venue,
+    C_comments_OSL,
+    C_comments_ROOT,
+    D1A_club_income_fund,
+    D1A_osl_seed_fund,
+    D1A_donation,
+    D1B_revenue,
+    D1B_donation_or_scholarship,
+    D1B_total_source_of_funds,
+    D11_items_goods_services,
+    D11_price,
+    D11_quantity,
+    D11_amount,
+    D11_total_revenue,
+    D2_items,
+    D2_reason_for_purchase,
+    D2_venue,
+    D2_total_expenditure,
+    D_comments_OSL,
+    D_comments_ROOT,
+    E_personal_data,
+    E_comments_OSL,
+    E_comments_ROOT,
+    F_name,
+    F_student_id,
+    F_position,
+    F_comments_OSL,
+    F_comments_ROOT,
+    G_1_1,
+    G_1_2,
+    G_1_3,
+    G_2_1,
+    G_2_2,
+    G_2_3,
+    G_3_1,
+    G_3_2,
+    G_3_3,
+    G_4_1,
+    G_4_2,
+    G_4_3,
+    G_5_1,
+    G_5_2,
+    G_5_3,
+    G_6_1,
+    G_6_2,
+    G_6_3,
+    G_7_1,
+    G_7_2,
+    G_7_3,
+    G_comments_OSL,
+    G_comments_ROOT,
+  ];
+
+  const query = `UPDATE EPFS SET (${columnNames}) = (${columnParams}) WHERE epf_id=$1`;
+  let result = await pool.query(query, values);
   await pool.end();
+  return result.rowCount;
 }
 
 export async function deleteEPF(epf_id) {
   const pool = new Pool(credentials);
-  const query = `DELETE FROM EPFS WHERE epf_id=${epf_id};`;
-  await pool.query(query);
+  const query = "DELETE FROM EPFS WHERE epf_id=$1";
+  const result = await pool.query(query, [epf_id]);
   await pool.end();
+  return result.rowCount;
 }
