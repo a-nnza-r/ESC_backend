2873193d2fa5b1d6e4c3513282162883434b9182
johnlohjy <johnlohjy@gmail.com>
2023-08-01 10:21:11 +0800

    Added robustness testing for EPFs in Jest

---
+++ test/epf_robustness_test/createEPFmiddleware.test.js
@@ -0,0 +1,76 @@
+import { 
+    validateJSON_createEPF, 
+    validateJSON_updateEPF, 
+    validateParam_getEPF, 
+    validateParam_deleteEPF 
+  } from "../../middleware/validationMiddleware.js";
+import path from "path";
+import fs from "fs";
+
+const jsonFilePath = path.join(__dirname, 'createEPF_testjson', 'createEPF_test1.json');
+const jsonData = fs.readFileSync(jsonFilePath, 'utf-8');
+const valid_req_body = JSON.parse(jsonData);
+
+// Mock Express request and response objects
+const mockRequest = (body) => ({ body });
+const mockResponse = () => {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
+
+describe('Robustness/Fuzzy testing of createEPF API', () => {
+
+
+
+  test('Test ID 1: Call next() when payload is valid', () => {
+    const req = mockRequest(valid_req_body);
+    const res = mockResponse();
+    const next = jest.fn();
+
+    validateJSON_createEPF(req, res, next);
+
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
+  });
+
+
+  test('Test ID 2: Return 400 error when JSON payload is incomplete', () => {
+    const req = mockRequest({
+      "status": "Pending",
+      "exco_user_id": "1",
+      "a_name": "user 1"});
+    const res = mockResponse();
+    const next = jest.fn();
+
+    validateJSON_createEPF(req, res, next);
+
+    expect(next).not.toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({ error: 'Invalid JSON payload format for createEPF' });
+  });
+
+  test('Test ID 3: Return 400 error when payload is invalid', () => {
+    const req = mockRequest("random");
+    const res = mockResponse();
+    const next = jest.fn();
+
+    validateJSON_createEPF(req, res, next);
+
+    expect(next).not.toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({ error: 'Invalid JSON payload format for createEPF' });
+  });
+
+
+
+
+
+});
+
+
+
+
+
---
+++ test/epf_robustness_test/getEPFmiddleware.test.js
@@ -0,0 +1,52 @@
+import { 
+    validateJSON_createEPF, 
+    validateJSON_updateEPF, 
+    validateParam_getEPF, 
+    validateParam_deleteEPF 
+  } from "../../middleware/validationMiddleware.js";
+
+// Mock Express request and response objects
+const mockRequest = (query) => ({ query });
+const mockResponse = () => {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
+
+describe('Robustness/Fuzzy testing of getEPF API', () => {
+
+
+  test('Test ID 1: Call next() when "epf_id" key exists in request query', () => {
+    const req = mockRequest({
+      epf_id: 1,
+    });
+    const res = mockResponse();
+    const next = jest.fn();
+
+    validateParam_getEPF(req, res, next);
+
+    expect(next).toHaveBeenCalledTimes(1);
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
+  });
+
+  test('Test ID 2: Return 400 error when "epf_id" key is missing from request query', () => {
+    const req = mockRequest({
+      // "epf_id" key is missing
+    });
+    const res = mockResponse();
+    const next = jest.fn();
+
+    validateParam_getEPF(req, res, next);
+
+    expect(next).not.toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({ error: 'Missing epf_id key for get EPF query' });
+  });
+
+
+});
+
+
+
